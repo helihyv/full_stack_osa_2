41a7c24564b55ad2955269a4a4060133c7c42730
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2018-08-08 12:09:11 +0300

    Tehtävä 2.5
    Tehtävä 2.5 valmis.

--- ekat/src/index.js
+++ ekat/src/index.js
@@ -1,55 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Kurssi from './components/Kurssi'
 
-const Otsikko = (props) => {
-  return(
-    <div>
-      <h1>{props.kurssi}</h1>
-    </div>
-  )
-}
 
-const Osa = (props) => {
-  return(
-    <div>
-      <p>{props.osa.nimi} {props.osa.tehtavia}</p>
-    </div>
-  )
-}
-
-const Sisalto = (props) => {
-  return(
-
-    <div>
-      {props.osat.map(osa => <Osa key={osa.id} osa={osa} />)}
-    </div>
-  )
-}
-
-const Yhteensa = ({ osat }) => {
-  const tehtaviaYhteensa = osat.reduce((yhteensa, osa) => {
-    return(
-      yhteensa + osa.tehtavia
-    )
-  },0)
-  return (
-    <div>
-      <p>
-        yhteensä {tehtaviaYhteensa} tehtävää
-      </p>
-    </div>
-  )
-}
-
-const Kurssi = ({kurssi}) => {
-  return (
-    <div>
-      <Otsikko kurssi={kurssi.nimi} />
-      <Sisalto osat={kurssi.osat} />
-      <Yhteensa osat = {kurssi.osat} />
-    </div>
-  )
-}
 
 const App = () => {
   const kurssit = [
